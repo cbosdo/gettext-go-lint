4a94be97e8bc96eb45d94661a1d8f921d9aa00ba
Cédric Bosdonnat <cbosdonnat@suse.com>
2024-04-30 11:19:11 +0200

    Remove now unused multiUnnamed variable

--- rules.go
+++ rules.go
@@ -5,17 +5,12 @@
 package main
 
 import (
-	"regexp"
-
 	"github.com/cbosdo/gettext-go-lint/rules"
 )
 
 var allRules []rules.Rule
 
-var multiUnnamed *regexp.Regexp
-
 func init() {
-	multiUnnamed = regexp.MustCompile("%[a-zA-Z0-9.]+")
 	allRules = []rules.Rule{
 		rules.MultiUnnamedVariables(),
 		rules.Ellipsis(),
